5e0f0802237c67d2758cc7546741ceb171cde87f
alfredo strubia <strubia.alfredo@gmail.com>
2023-11-29 16:00:22 -0300

    segunda pre-entrega

--- js/index.js
+++ js/index.js
@@ -1,41 +1,57 @@
-function priorizarValor (sistolica, diastolica) {
-    const rangoSistolica = calcularRangoSistolica (sistolica);
-    const rangoDiastolica = calcularRangoDiastolica (diastolica);
+let objetoPresion = {
+  priorizarValor: function(sistolica, diastolica) {
+    const rangoSistolica = this.calcularRangoSistolica(sistolica);
+    const rangoDiastolica = this.calcularRangoDiastolica(diastolica);
 
-    return Math.max (rangoSistolica, rangoDiastolica);
-}
+    return Math.max(rangoSistolica, rangoDiastolica);
+  },
 
-function calcularRangoSistolica (sistolica) {
+  calcularRangoSistolica: function(sistolica) {
     if (sistolica >= 100 && sistolica <= 140) {
-        return "tension normal";
+      return "tension normal";
     } else if (sistolica >= 141 && sistolica <= 160) {
-        return "hipertension";
+      return "hipertension";
     } else if (sistolica >= 161 && sistolica <= 180) {
-        return "hipertension severa";
+      return "hipertension severa";
     } else if (sistolica > 181) {
-        return "crisis hipertensiva";
+      return "crisis hipertensiva";
     } else {
-        return "el valor ingresado no es valido";
+      return "el valor ingresado no es valido";
     }
-}
+  },
 
-function calcularRangoDiastolica (diastolica) {
+  calcularRangoDiastolica: function(diastolica) {
     if (diastolica >= 60 && diastolica <= 90) {
-        return "tension normal";
+      return "tension normal";
     } else if (diastolica >= 91 && diastolica <= 100) {
-        return "hipertension";
+      return "hipertension";
     } else if (diastolica >= 101 && diastolica <= 110) {
-        return "hipertension severa";
+      return "hipertension severa";
     } else if (diastolica > 111) {
-        return "crisis hipertensiva";
+      return "crisis hipertensiva";
     } else {
-        return "el valor ingresado no es valido";
+      return "el valor ingresado no es valido";
     }
-}
+  },
+
+  resultadosArray: [],
+
+  agregarResultado: function(resultado) {
+    this.resultadosArray.push(resultado);
+  },
+
+  obtenerResultados: function() {
+    return this.resultadosArray;
+  }
+};
 
 let sistolica = parseFloat(prompt("Ingrese el valor de su presion sistolica:"));
 let diastolica = parseFloat(prompt("Ingrese el valor de su presion diastolica:"));
 
-let resultado = priorizarValor (sistolica, diastolica);
+let resultado = objetoPresion.priorizarValor(sistolica, diastolica);
+objetoPresion.agregarResultado(resultado);
 
-alert (resultado);
+let resultados = objetoPresion.obtenerResultados();
+
+alert(resultados);
+
