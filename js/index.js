3fb9ac3bc90d8f8da556d4325ac63d2cf302b367
alfredo strubia <strubia.alfredo@gmail.com>
2023-12-13 23:03:33 -0300

    tercera pre entrega js

--- js/index.js
+++ js/index.js
@@ -8,29 +8,46 @@ let objetoPresion = {
 
   calcularRangoSistolica: function(sistolica) {
     if (sistolica >= 100 && sistolica <= 140) {
-      return "tension normal";
+      return 1; 
     } else if (sistolica >= 141 && sistolica <= 160) {
-      return "hipertension";
+      return 2; 
     } else if (sistolica >= 161 && sistolica <= 180) {
-      return "hipertension severa";
+      return 3; 
     } else if (sistolica > 181) {
-      return "crisis hipertensiva";
+      return 4; 
     } else {
-      return "el valor ingresado no es valido";
+      return 0; 
+    }
+  },
+  
+  calcularRangoDiastolica: function(diastolica) {
+    if (diastolica >= 60 && diastolica <= 90) {
+      return 1; 
+    } else if (diastolica >= 91 && diastolica <= 100) {
+      return 2; 
+    } else if (diastolica >= 101 && diastolica <= 110) {
+      return 3; 
+    } else if (diastolica > 111) {
+      return 4; 
+    } else {
+      return 0; 
     }
   },
 
-  calcularRangoDiastolica: function(diastolica) {
-    if (diastolica >= 60 && diastolica <= 90) {
-      return "tension normal";
-    } else if (diastolica >= 91 && diastolica <= 100) {
-      return "hipertension";
-    } else if (diastolica >= 101 && diastolica <= 110) {
-      return "hipertension severa";
-    } else if (diastolica > 111) {
-      return "crisis hipertensiva";
-    } else {
-      return "el valor ingresado no es valido";
+  obtenerRespuesta: function(rango) {
+    switch (rango) {
+      case 0:
+        return "Valor inválido";
+      case 1:
+        return "Tensión normal";
+      case 2:
+        return "Hipertensión";
+      case 3:
+        return "Hipertensión severa";
+      case 4:
+        return "Crisis hipertensiva";
+      default:
+        return "Respuesta no encontrada";
     }
   },
 
@@ -45,13 +62,48 @@ let objetoPresion = {
   }
 };
 
-let sistolica = parseFloat(prompt("Ingrese el valor de su presion sistolica:"));
-let diastolica = parseFloat(prompt("Ingrese el valor de su presion diastolica:"));
+function handleFormSubmit(event) {
+  event.preventDefault();
 
-let resultado = objetoPresion.priorizarValor(sistolica, diastolica);
-objetoPresion.agregarResultado(resultado);
+  let sistolicaInput = document.querySelector("#sistolica").value;
+  let diastolicaInput = document.querySelector("#diastolica").value;
+  
 
-let resultados = objetoPresion.obtenerResultados();
+  if (!isNaN(sistolicaInput) && !isNaN(diastolicaInput)) {
+    sistolicaInput = parseFloat(sistolicaInput);
+    diastolicaInput = parseFloat(diastolicaInput);
 
-alert(resultados);
+    let resultado = objetoPresion.priorizarValor(sistolicaInput, diastolicaInput);
+    objetoPresion.agregarResultado(resultado);
 
+    let respuesta = objetoPresion.obtenerRespuesta(resultado);
+
+    sessionStorage.setItem("sistolica", sistolicaInput);
+    sessionStorage.setItem("diastolica", diastolicaInput);
+    sessionStorage.setItem("respuesta", respuesta);
+
+    let respuestaElement = document.querySelector("#respuesta");
+    respuestaElement.textContent = respuesta;
+
+    document.getElementById("presionForm").reset();
+
+  } else {
+    alert("Por favor, ingrese valores numéricos para la presión sistólica y diastólica.");
+  } 
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("#presionForm").addEventListener("submit", handleFormSubmit.bind(null, event));
+
+
+  let sistolica = sessionStorage.getItem("sistolica");
+  let diastolica = sessionStorage.getItem("diastolica");
+  let respuesta = sessionStorage.getItem("respuesta");
+
+  if (sistolica && diastolica && respuesta) {
+    document.querySelector("#sistolica").value = sistolica;
+    document.querySelector("#diastolica").value = diastolica;
+    document.querySelector("#respuesta").textContent = respuesta;
+  }
+});
